7a1c75b405490f330aee6cb51b0cfe73f6415b93
sidisthegoat <sudhangrajpurohitt@gmail.com>
2026-01-24 19:18:21 +0530

    chore: update codebase and fix exports

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import HeroSection from "@/components/HeroSection";
 import ProductSection from "@/components/ProductSection";
 import CategorySection from "@/components/CategorySection";
